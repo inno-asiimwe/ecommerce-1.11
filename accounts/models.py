16a28572cd50fe93bf608f65e5135fbe22fb7853
Innocent Asiimwe <innocent@Innocents-MacBook-Pro.local>
2018-01-17 02:34:06 +0300

    [Feature #54310359] Add a custom user model.

---
+++ accounts/models.py
@@ -0,0 +1,87 @@
+""" Module contains account models """
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager
+)
+
+
+class UserManager(BaseUserManager):
+    """ Model manager for our custom user model"""
+    def create_user(self, email, password=None):
+        """Creates and saves a new user with the given email and password"""
+        if not email:
+            raise ValueError("Users must have an email")
+        if not password:
+            raise ValueError("User must have a password")
+
+        user_obj = self.model(
+            email=self.normalize_email(email)
+        )
+
+        user_obj.set_password(password)
+        user_obj.save(using=self._db)
+        return user_obj
+
+    def create_staffuser(self, email, password):
+        """ Creates and saves a staff user """
+        user_obj = self.create_user(email, password=password)
+        user_obj.staff = True
+        user_obj.save(using=self._db)
+        return user_obj
+
+    def create_superuser(self, email, password):
+        """ Create and save a super user """
+        user_obj = self.create_user(email, password=password)
+        user_obj.staff = True
+        user_obj.admin = True
+        user_obj.active = True
+        user_obj.save(using=self._db)
+        return user_obj
+
+
+class User(AbstractBaseUser):
+    """Custom user model"""
+    email = models.EmailField(max_length=255, unique=True)
+    active = models.BooleanField(default=False)
+    staff = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def get_full_name(self):
+        """" User is identified by their email address"""
+        return self.email
+
+    def get_short_name(self):
+        """" User is identified by their email address"""
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        """ Does the user have a specific permission"""
+        return True
+    
+    def has_module_perms(self, app_label):
+        """Does the user have permissions to view the app 'app_label' """
+        return True
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def is_admin(self):
+        """ Is the user an administrator """
+        return self.admin
+
+    @property
+    def is_staff(self):
+        """ Is the user a member of staff """
+        return self.staff
+
+    @property
+    def is_active(self):
+        """ Is the user account active """
+        return self.active
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -80,6 +81,8 @@ DATABASES = {
     }
 }
 
+# User model
+AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
