f9d2bb3d095c35369cf41c503f2dafdaeffc971d
Innocent Asiimwe <innocent@Innocents-MacBook-Pro.local>
2018-01-17 03:40:55 +0300

    [Feature #54310359] Add forms for admin user creation and update.

---
+++ accounts/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+from .models import User
+
+
+class UserAdminCreationForm(forms.ModelForm):
+    """ Form for creating users via django admin """
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Confirm Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email',)
+
+    def clean_password2(self):
+        """ Ensure password match """
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('passwords do not match')
+        return password2
+
+    def save(self, commit=True):
+        """ Hash Password """
+        user = super(UserAdminCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserAdminChangeForm(forms.ModelForm):
+    """ Form for Updating User """
+
+    password = ReadOnlyPasswordHashField
+
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'active', 'admin')
+
+    def clean_password(self):
+        """ Return initial password """
+        return self.initial["password"]
